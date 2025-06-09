ce9c8b8068a87c76be966aabe9280c4c63d71983
Emmanuelbagbi <emmanuelbagbi4@gmail.com>
2025-06-09 17:44:01 +0100

    initial commit

--- models/User.js
+++ models/User.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 
+
+
 const userSchema = new mongoose.Schema({
     _id:{ type : String, required:true},
     name:{ type : String, required:true},
-    email:{ type : String, required:true, unique},
+    email:{ type : String, required:true, unique:true},
     ImageUrl : { type : String, required:true},
     cartItems: { type:Object, default:{}}
 
