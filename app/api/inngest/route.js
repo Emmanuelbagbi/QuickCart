0062bd499bec5b63fcbea6da2a46536e91f6ec48
Emmanuelbagbi <emmanuelbagbi4@gmail.com>
2025-06-10 20:10:33 +0100

    QuickCart

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,5 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncuserUpdation } from "@/config/inngest";
+import { createUserOrder, inngest, syncUserCreation, syncUserDeletion, syncuserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -7,6 +7,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     syncUserCreation,
     syncuserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    createUserOrder
   ],
 });
